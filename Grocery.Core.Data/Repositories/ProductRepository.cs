5f52c34fa8aa1f7382a94dbf64748e35a863bb0a
yamen <s1211665@student.windesheim.nl>
2025-09-10 12:45:57 +0200

    UC3 afgerond: de lijst van producten is in de constructor ingevuld en de methode 'GetAll' geeft de producten correct weer.

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -16,11 +16,16 @@ namespace Grocery.Core.Data.Repositories
                  * Brood[voorraad 400] en 
                  * Cornflakes[voorraad 0]
                 */
-				];
+                new Product(1, "Melk", 300),
+                new Product(2, "Kaas", 100),
+                new Product(3, "Brood", 400),
+                new Product(4, "Cornflakes", 0)
+                ];
         }
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return [];
+            //return products; //wijzig dit in de gemaakte lijst uit de constructo
         }
 
         public Product? Get(int id)
